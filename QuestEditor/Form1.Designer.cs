af9280c83cff14fa2ecd7311f6313fb85776399d
dugging1 <goldminergaming@aol.com>
2017-12-07 23:47:00 +0000

    Add project files.

---
+++ QuestEditor/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace QuestEditor {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing&&(components!=null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Quests");
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.btnNewQueset = new System.Windows.Forms.ToolStripButton();
+			this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			this.toolStripContainer1.ContentPanel.SuspendLayout();
+			this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+			this.toolStripContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+			this.splitContainer1.Size = new System.Drawing.Size(1026, 746);
+			this.splitContainer1.SplitterDistance = 229;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeView1.Location = new System.Drawing.Point(0, 0);
+			this.treeView1.Name = "treeView1";
+			treeNode2.Name = "TreeRoot";
+			treeNode2.Text = "Quests";
+			this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+			this.treeView1.Size = new System.Drawing.Size(229, 746);
+			this.treeView1.TabIndex = 0;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNewQueset});
+			this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(81, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// btnNewQueset
+			// 
+			this.btnNewQueset.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.btnNewQueset.Image = ((System.Drawing.Image)(resources.GetObject("btnNewQueset.Image")));
+			this.btnNewQueset.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.btnNewQueset.Name = "btnNewQueset";
+			this.btnNewQueset.Size = new System.Drawing.Size(69, 22);
+			this.btnNewQueset.Text = "New Quest";
+			// 
+			// toolStripContainer1
+			// 
+			// 
+			// toolStripContainer1.ContentPanel
+			// 
+			this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+			this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1026, 746);
+			this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+			this.toolStripContainer1.Name = "toolStripContainer1";
+			this.toolStripContainer1.Size = new System.Drawing.Size(1026, 771);
+			this.toolStripContainer1.TabIndex = 2;
+			this.toolStripContainer1.Text = "toolStripContainer1";
+			// 
+			// toolStripContainer1.TopToolStripPanel
+			// 
+			this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1026, 771);
+			this.Controls.Add(this.toolStripContainer1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+			this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+			this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+			this.toolStripContainer1.ResumeLayout(false);
+			this.toolStripContainer1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton btnNewQueset;
+		private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+	}
+}
+
---
+++ QuestEditor/QuestEntry.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestEditor {
+	internal struct QuestEntry {
+		public string Label { get; set; }
+		public QuestLocationEntry[] Locations { get; set; }
+
+		public static QuestEntry DecodeJson(string json) {
+			QuestEntry s = JsonConvert.DeserializeObject<QuestEntry>(json);
+
+			for (int i = 0; i<s.Locations.Length; i++) {
+				s.Locations[i].TrueChoices=new QuestChoicesEntry[s.Locations[i].Choices.Count];
+				for (int p = 0; p<s.Locations[i].Choices.Count; p++) {
+					s.Locations[i].TrueChoices[p]=JsonConvert.DeserializeObject<QuestChoicesEntry>(s.Locations[i].Choices[p].ToString());
+				}
+			}
+			return s;
+		}
+	}
+
+	internal struct QuestLocationEntry {
+		public string Name { get; set; }
+		public string Label { get; set; }
+		public Dictionary<string,string>[] Requirements { get; set; }
+		public string Description { get; set; }
+		public QuestChoicesEntry[] TrueChoices { get; set; }
+
+		public List<JObject> Choices { get; set; }
+	}
+
+	internal struct QuestChoicesEntry {
+		public string Label { get; set; }
+		public string Description { get; set; }
+		public QuestChoicesEntry[] Choices { get; set; }
+		public Dictionary<string, string>[] Requirements { get; set; }
+		public Dictionary<string, string>[] Events { get; set; }
+	}
+}
